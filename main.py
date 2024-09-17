0753ef2d90663382676b3569b103933a1313ab75
achref habib rahali <rahaliashraf732@gmail.com>
2024-09-17 19:22:21 +0100

    wiwi

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import os
 import glob
+import shutil
+
+extension_types = {
+    'images': ['jpg', 'png', 'webp'],
+    'documents': ['pdf', 'docx', 'xlsx', 'csv', 'txt'],
+    'archives': ['zip', 'tar', 'rar'],
+    'executables': ['exe', 'msi', 'jar', 'whl'],
+    'configuration': ['ini'],
+    'torrent': ['torrent']
+}
+
+def get_category(extension):
+    for category, ext_list in extension_types.items():
+        if extension in ext_list:
+            return category
+    return 'others'
 
 # Change the current working directory to "C:/Users/rahal/Downloads"
 os.chdir("C:/Users/rahal/Downloads")
@@ -7,22 +23,27 @@ os.chdir("C:/Users/rahal/Downloads")
 current_directory = os.getcwd()  # Get the updated current directory
 print("Current directory:", current_directory)
 
-pdf_files = glob.glob(os.path.join(current_directory, "*.pdf"))
-number_of_pdf_files = len(pdf_files)
-print("Number of PDF files in the current directory:", number_of_pdf_files)
+files = os.listdir(current_directory)
 
-jpeg_files = glob.glob(os.path.join(current_directory, "*.jpeg"))
-number_of_jpeg_files = len(jpeg_files)
-print("Number of jpeg files in the current directory:", number_of_jpeg_files)
+extensions = []
+folders = []
 
-jpg_files = glob.glob(os.path.join(current_directory, "*.jpg"))
-number_of_jpg_files = len(jpg_files)
-print("Number of jpg files in the current directory:", number_of_jpg_files)
+for file in files :
+    if(not os.path.isdir(f"{current_directory}/{file}")):
+        e = file.split(".")
+        x = e[len(e)-1]
+        folder = f'{current_directory}/{get_category(x)}'
+        if(not os.path.isdir(folder)):
+            os.mkdir(folder)
+            shutil.move(f"{current_directory}/{file}",f"{current_directory}/{get_category(x)}/{file}")
+        else :
+            shutil.move(f"{current_directory}/{file}",f"{current_directory}/{get_category(x)}/{file}")
+        
+    else:
+        if(not os.path.isdir(f'{current_directory}/folders')):
+            os.mkdir(f'{current_directory}/folders')
+            shutil.move(f"{current_directory}/{file}",f"{current_directory}/folders/{file}")
+        else :
+            shutil.move(f"{current_directory}/{file}",f"{current_directory}/folders/{file}")
+        folders.append(file)
 
-png_files = glob.glob(os.path.join(current_directory, "*.png"))
-number_of_png_files = len(jpg_files)
-print("Number of png files in the current directory:", number_of_png_files)
-
-folders = [f for f in os.listdir(current_directory) if os.path.isdir(f)]
-number_of_folders = len(folders)
-print("Number of folders in the current directory:", number_of_folders)
